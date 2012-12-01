bd9c550e8c70aefb972c8e804e821b7fdc700cb1
Christofer Dutz <cdutz@apache.org>
2012-12-01 22:43:30 +0000

    Initial import.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/flex/utilities/trunk@1416068 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-flex-plugin/trunk/plugins/maven-flex-plugin/src/main/java/org/apache/flex/maven/plugins/flex/AbstractFlexMojo.java
@@ -0,0 +1,12 @@
+package org.apache.flex.maven.plugins.flex;
+
+import org.apache.maven.plugin.AbstractMojo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cdutz
+ * Date: 01.12.12
+ * Time: 18:24
+ */
+public abstract class AbstractFlexMojo extends AbstractMojo {
+}
