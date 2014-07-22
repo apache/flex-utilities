48828f0ae6efcd0b29ff33566ee86c7c33c39628
Justin Mclean <jmclean@apache.org>
2014-07-23 08:26:58 +1000

    Converted to unix line ending and added $FLEX_HOME. Probably should be converted to ant

--- TourDeFlex/TourDeFlex3/src/build.sh
+++ TourDeFlex/TourDeFlex3/src/build.sh
@@ -17,11 +17,11 @@
 
 OPTS=-use-network=false
 
-../../bin/mxmlc $OPTS explorer.mxml
+$FLEX_HOME/bin/mxmlc $OPTS explorer.mxml
 
 mxmlFiles=`find */* -name '*.mxml' -print`
 
 for mxml in ${mxmlFiles}; do
         echo "building $mxml"
-        (../../bin/mxmlc $OPTS ${mxml})
+        ($FLEX_HOME/bin/mxmlc $OPTS ${mxml})
 done
