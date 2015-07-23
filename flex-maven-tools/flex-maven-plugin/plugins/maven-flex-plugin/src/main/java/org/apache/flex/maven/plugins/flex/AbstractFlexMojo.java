8bc0350d2c8f61ded3f565191c41e5e604353756
Christofer Dutz <christofer.dutz@codecentric.de>
2015-07-23 11:30:09 +0200

    - Renamed the root directory - Renamed the artifact of the maven-extension to flex-sdk-converter-maven-extension

---
+++ flex-maven-tools/flex-maven-plugin/plugins/maven-flex-plugin/src/main/java/org/apache/flex/maven/plugins/flex/AbstractFlexMojo.java
@@ -0,0 +1,6 @@
+package org.apache.flex.maven.plugins.flex;
+
+import org.apache.maven.plugin.AbstractMojo;
+
+public abstract class AbstractFlexMojo extends AbstractMojo {
+}
--- flex-maven-tools/maven-flex-plugin/plugins/maven-flex-plugin/src/main/java/org/apache/flex/maven/plugins/flex/AbstractFlexMojo.java
+++
@@ -1,6 +0,0 @@
-package org.apache.flex.maven.plugins.flex;
-
-import org.apache.maven.plugin.AbstractMojo;
-
-public abstract class AbstractFlexMojo extends AbstractMojo {
-}
